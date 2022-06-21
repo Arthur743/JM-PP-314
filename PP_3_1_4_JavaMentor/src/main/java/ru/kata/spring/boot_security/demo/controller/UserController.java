324d90c411a9e54561562737ccf59be6c440eae3
Arthur743 <100314214+Arthur743@users.noreply.github.com>
2022-06-21 10:44:02 +0300

    Update UserController.java

--- PP_3_1_4_JavaMentor/src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ PP_3_1_4_JavaMentor/src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -27,7 +27,7 @@ public class UserController {
     @GetMapping("/user/user")
     public String showInfoOfUser(Model model, Principal principal) {
         model.addAttribute("activeUser", userService.getSpecificUser(principal.getName()));
-        return "users";
+        return "user";
     }
 
     @GetMapping("/admin/users")
